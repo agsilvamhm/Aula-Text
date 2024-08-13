189f5971f661871d823094fc48079dc95aa1cd02
Adalberto Gonçalves da Silva <agsilva.mhm@gmail.com>
2024-08-13 08:03:52 -0300

    Projeto impolementado

--- app/src/main/java/com/agsilva/aula_text/MainActivity.java
+++ app/src/main/java/com/agsilva/aula_text/MainActivity.java
@@ -1,6 +1,9 @@
 package com.agsilva.aula_text;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editNome;
+    private EditText editSobrenome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,34 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        editNome = findViewById(R.id.editNome);
+        editSobrenome = findViewById(R.id.editSobrenome);
+    }
+
+    public void limpar(View view){
+        editNome.setText(null);
+        editSobrenome.setText(null);
+
+        editNome.requestFocus();
+
+        Toast.makeText(this, "Campos limpos!", Toast.LENGTH_SHORT).show();
+    }
+
+    public void mostrarNomecompleto(View view){
+        String nome = editNome.getText().toString();
+        String sobrenome = editSobrenome.getText().toString();
+
+        if (nome == null || nome.trim().isEmpty()){
+            Toast.makeText(this, R.string.nome_vazio, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (sobrenome == null || sobrenome.trim().isEmpty()){
+            Toast.makeText(this, R.string.sobrenome_vazio, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Toast.makeText(this, nome + " " + sobrenome, Toast.LENGTH_LONG).show();
     }
 }
